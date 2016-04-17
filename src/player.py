a827c43a4aac3e7f5ed47488e40152640b8c1c99
Ruan <ruanvictorreis@gmail.com>
2016-04-17 00:07:52 -0300

    Rotation of hunter

--- src/player.py
+++ src/player.py
@@ -3,23 +3,32 @@ class Player (object):
 	def __init__(self):
 		self.position_x = 0
 		self.position_y = 0
+		self.direction = "FRONT"
 		self.image = "../res/images/hunter/hunter_front.png"
 	
 	def position(self):
 		return (self.x, self.y)
 	
 	def increment_x(self):
-		self.position_x = self.position_x + 1
 		self.image = "../res/images/hunter/hunter_right.png"
+		if(self.direction == "RIGHT"):
+			self.position_x = self.position_x + 1
+		self.direction = "RIGHT"
 
 	def decrement_x(self):
-		self.position_x = self.position_x - 1
 		self.image = "../res/images/hunter/hunter_left.png"
+		if(self.direction == "LEFT"):
+			self.position_x = self.position_x - 1
+		self.direction = "LEFT"
 		
 	def increment_y(self):
-		self.position_y = self.position_y + 1
 		self.image = "../res/images/hunter/hunter_front.png"
+		if(self.direction == "FRONT"):
+			self.position_y = self.position_y + 1
+		self.direction = "FRONT"
 
 	def decrement_y(self):
-		self.position_y = self.position_y - 1
 		self.image = "../res/images/hunter/hunter_back.png"
+		if(self.direction == "BACK"):
+			self.position_y = self.position_y - 1
+		self.direction = "BACK"
