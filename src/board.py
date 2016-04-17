1f0491b286d50c0745f3bef4c5c851bfce229f80
Ruan <ruanvictorreis@gmail.com>
2016-04-16 23:13:28 -0300

    Refactor images

--- src/board.py
+++ src/board.py
@@ -4,8 +4,8 @@ class Board (object):
 		self.matrix = self.init_matrix()
 		self.score = 0
 		self.spacing = 10
-		self.cell_dimension = 150
-		self.matrix_dimension = (4,4)
+		self.cell_dimension = 100
+		self.matrix_dimension = (6,6)
 		self.width = (self.cell_dimension * self.matrix_dimension[0]) + (self.spacing * (self.matrix_dimension[0] + 1) )
 		self.height = (self.cell_dimension * self.matrix_dimension[1]) + (self.spacing * (self.matrix_dimension[1] + 1) )
 	
